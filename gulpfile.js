9d07bd0791344b33c4950eebcb9991f0d5b58c43
clmntnbrn <clementine.brown@red-badger.com>
2019-08-07 10:54:50 +0100

    adding cv experience

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,24 @@
 var gulp = require('gulp');
+// var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+// sass.compiler = require('node-sass');
+
+// gulp.task('default', function() {
+//   // we want to run "sass css/app.scss app.css --watch"
+//   return gulp.src("css/app.scss")
+//     .pipe(sass())
+//     .pipe(gulp.dest("app.css"))
+// });
 
 var browserSync = require('browser-sync').create()
 
-gulp.watch()
+gulp.task("watch", function (){
+
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task("default", ["watch"])
